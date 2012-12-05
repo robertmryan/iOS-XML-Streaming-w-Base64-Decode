67a6297feb50868864d5caff6c2cf6a18501f9a9
Robert Ryan <rryan@Robert-Ryans-iMac.local>
2012-12-05 16:32:58 -0500

    Initial Commit

---
+++ Streaming XML/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Streaming XML
+//
+//  Created by Robert Ryan on 12/5/12.
+//  Copyright (c) 2012 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Streaming XML/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Streaming XML
+//
+//  Created by Robert Ryan on 12/5/12.
+//  Copyright (c) 2012 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
